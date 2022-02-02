c33ba8629c76b26376c63ca626741b4fd4893e04
qgatssdev <chivoomodu@gmail.com>
2022-02-02 13:39:38 +0100

    still eok

--- src/components/homepage.component.jsx
+++ src/components/homepage.component.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
+
 import '../Sass/homepage.styles.scss';
 
-const Homepage = () => (
+const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menue">
+    <h1>Welcome to my Homepage</h1>
+    <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">HATS</h1>
+          <div className="title">HATS</div>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
-    <div className="directory-menue">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">HATS</h1>
+          <div className="title">JACKETS</div>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
-    <div className="directory-menue">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">HATS</h1>
+          <div className="title">SHOES</div>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
-    <div className="directory-menue">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">HATS</h1>
+          <div className="title">WOMENS</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">MENS</div>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
@@ -38,4 +40,4 @@ const Homepage = () => (
   </div>
 );
 
-export default Homepage;
+export default HomePage;
