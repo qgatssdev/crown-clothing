5bfe8027b393bb894649eb0929752d8738aa70fa
qgatssdev <chivoomodu@gmail.com>
2022-02-02 13:05:08 +0100

    nn

--- src/components/homepage.component.jsx
+++ src/components/homepage.component.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import '../Sass/homepage.styles.scss';
 
 const Homepage = () => (
   <div className="homepage">
+    <div className="directory-menue">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menue">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menue">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
     <div className="directory-menue">
       <div className="menu-item">
         <div className="content">
