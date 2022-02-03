f24e4cdd01dc2ae43bd78dbe15c6e2393f91329f
qgatssdev <chivoomodu@gmail.com>
2022-02-03 17:31:03 +0100

    upgraded router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
- 
